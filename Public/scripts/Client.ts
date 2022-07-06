1cc1e9f07cf69ca64ddf81234dd4be5dcdd4d542
Dimaschine <dmytro.skorbyashchenskyy@mni.thm.de>
2022-07-06 08:51:31 +0200

    verbesserung Server und Umlenkung Pfad .html

--- Public/scripts/Client.ts
+++ Public/scripts/Client.ts
@@ -146,7 +146,7 @@ document.addEventListener("DOMContentLoaded",()=>{
               
               
           }         
-                    
+ //
                     
                     
                     
--- Server.ts
+++ Server.ts
@@ -23,5 +23,38 @@ connection.connect((err) => {
     }
 });
 
- 
+// Ein Wrapper von Manuel Groh, um die MySQL-Query als Promise (then/catch Syntax) zu nutzen
+function query(sql: string, param: any[] = []): Promise<any> {
+    return new Promise<any>((resolve: any, reject: any) => {
+        connection.query(sql, param, (err: mysql.MysqlError | null, results: any) => {
+            if (err === null) {
+                resolve(results);
+            } else {
+                reject(err);
+            }
+        });
+    });
+}
 
+const app: express.Express = express();
+app.listen(8080);
+//notwendige übergabeparameter für JSON und URLencoded
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+//Implementierung Session
+app.use(session({
+    cookie: {
+        maxAge: 60 * 60 * 1000,
+        sameSite: true
+    },
+    secret: Math.random().toString(),
+    resave: false,
+    saveUninitialized: false,
+}));
+// __dirname+"/Name des Ordners der umgeleitet wird"
+app.use("/cMe",express.static(__dirname+"/Public" ));
+//Sendet .html
+app.get("/",(req: express.Request, res: express.Response)=>{
+    res.status(200);
+    res.sendFile( __dirname + "/Public/index.html");
+});
