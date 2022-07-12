e0c1cf4935d39f5a4fa63f8b6546d096f4cb0e6f
artir <artir.guri@mni.thm.de>
2022-07-12 10:01:04 +0200

    deleteNachricht Serverseitig geschrieben

--- Server.ts
+++ Server.ts
@@ -89,6 +89,8 @@ app.post("/benutzer",postBenutzer);
 app.get("/benutzer/:email",checkLogin,getBenutzer);
 app.delete("/benutzer/:email",checkLogin,deleteBenutzer);
 app.put("/benutzer/:email",checkLogin,putBenutzer);
+//Routen Nachricht
+app.delete("/nachrichten/:inhalt",checkLogin,deleteNachricht);
 
 ///Alle Log- in 'n - out funktionen
 
@@ -255,5 +257,29 @@ function putBenutzer(req: express.Request, res:express.Response):void{
 
 }
 
+//Funktionen Nachricht 9:00
+function deleteNachricht(req: express.Request, res:express.Response):void{
+    //warum fehler?
+    const inhalt: string = req.params.inhalt;
+    const email: string = req.session.uname;
+
+    const param = [inhalt, email];
+    const sql = "DELETE FROM nachrichten WHERE inhalt = ? AND email = ?"
+
+    if(inhalt === undefined){
+        res.status(400);
+        res.send("Kein zu löschender Inhalt");
+    }else if(inhalt){
+        connection.query(
+            sql,
+            param,
+            (err:mysql.MysqlError| null, results: any)=>{
+                res.status(200);
+                res.send("Nachricht gelöscht")
+            }
+        )
+    }
+
+}
 
 
